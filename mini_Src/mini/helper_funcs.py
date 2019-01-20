31340fe6aaee9049157520798fbb6428aa516096
eyitav <eyitav@post.bgu.ac.il>
2019-01-20 13:53:50 -0500

    update 20.1.2019 20:53

--- mini_Src/mini/helper_funcs.py
+++ mini_Src/mini/helper_funcs.py
@@ -7,7 +7,7 @@ Created on 2 Dec 2018
 '''helper functions'''
 from _functools import reduce
 import re
-
+'''updates a single index-i which corresponds to value-value in the node root'''
 def map_iterator_int(value,i,root):
     children = root["children"] 
     if  children == []:
@@ -27,6 +27,7 @@ def map_iterator_int(value,i,root):
                     to_add = i
                     root.get('index').append(to_add)
     '''
+'''updates a single index-i which corresponds to value-value in the node root'''
 def map_iterator_str(value,i,root): 
     children = root["children"] 
     if  children == []:
@@ -45,7 +46,8 @@ def map_iterator_str(value,i,root):
                 if value.lower() == root.get('data').lower(): 
                     to_add = i
                     root.get('index').append(to_add)
-    '''                     
+    ''' 
+'''returns the size number of the node's-node support'''                    
 def sum_of_supports(node):
     if node.get('children') == []:
         length=len(node.get('index'))
@@ -54,7 +56,7 @@ def sum_of_supports(node):
     lst=list(map(sum_of_supports,node.get('children')))    
     summ = reduce(lambda x,y: x+y,lst) 
     return summ
-
+'''returns the set of the node's-node support'''
 def set_of_supports(node):
     if node.get('children') == []:
         return set(node.get('index'))
@@ -62,25 +64,27 @@ def set_of_supports(node):
     lst=list(map(set_of_supports,node.get('children')))    
     output_set = reduce(lambda x,y: x.union(y),lst) 
     return output_set
-
+'''
 def intersection_support(child, root):
     support_child = set_of_supports(child)
     support_root = set_of_supports(root)
     return support_child.intersection(support_root)
-
+'''
+'''tags nodes which their support is less than k with deleted mark - stops the current depth scan when reaching such a node'''
 def remove_nodes_under_k(node,k):
     if sum_of_supports(node) < k :
         node.update({'isDeleted' : True})
     else:
         node.update({'isDeleted' : False})
         list(map(lambda n:remove_nodes_under_k(n,k) , node.get('children')))
-        
+'''       
 def is_set_in_set(the_out_set,the_in_set):
     for item in the_out_set:
         if(the_in_set == item):
             return True
     return False
-
+'''
+'''returns the number of leaves in a node - is used in algorithm 4 in the computation of the cost function'''
 def number_of_leaves(node):
     if node.get('children') == []:
         return 1
@@ -88,7 +92,8 @@ def number_of_leaves(node):
     lst=list(map(number_of_leaves,node.get('children')))    
     summ = reduce(lambda x,y: x+y,lst) 
     return summ
-
+'''computes the generalized sets of the closed frequent sets(F_CF(denoted by S)) saves each R(j) in a set of its own 
+   the combined row is R(j) 0<=j<=len(attributes_array_by_order_database) and there is a row for each set in F_CF(denoted by S)'''
 def generalize_S(database_path, S, relevant_indexes,attributes_array_by_order_database):
     generalized_records = [[set() for i in range(0,len(attributes_array_by_order_database))] for j in range(0,len(S)) ] 
     with open(database_path) as f: 
